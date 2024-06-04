7b57ec9d78a49da99b6c1b07c467946fff10f97a
yopaseopor <yopaseopor@gmail.com>
2024-06-04 23:37:34 +0200

    via_ferrata

--- src/config.js
+++ src/config.js
@@ -214,6 +214,43 @@ style: function (feature) {
 				},
 			
 		{
+			group: 'Deportes',
+			title: 'Vía ferrata',
+			query: '(nwr["highway"="via_ferrata"]({{bbox}});node(w););out meta;',
+			iconSrc: 'icones/via_ferrata.svg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: 'icones/via_ferrata.svg',
+							scale:0.30
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+			
+		{
 			group: 'Marcas & Economía',
 			title: 'Banco Sabadell',
 			query: '(nwr["brand:wikidata"="Q762330"]({{bbox}});node(w););out meta;',
